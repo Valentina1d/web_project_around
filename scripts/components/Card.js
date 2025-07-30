f7c62ea13c5d8e150e9b3ac3814272c83a9a8f90
unknown <valenerpr@unicauca.edu.co>
2025-07-30 13:43:55 -0500

    Sprint 11

--- scripts/components/Card.js
+++ scripts/components/Card.js
@@ -4,13 +4,13 @@ export class Card {
     this._link = data.link;
     this._templateSelector = templateSelector;
   }
-
+ 
   _getTemplate() {
     const template = document.querySelector(this._templateSelector).content;
     const cardElement = template.querySelector('.card').cloneNode(true);
     return cardElement;
   }
-
+ 
   _setEventListeners() {
     this._likeButton.addEventListener('click', () => {
       const currentSrc = this._likeIcon.getAttribute('src');
@@ -22,44 +22,45 @@ export class Card {
         this._likeIcon.setAttribute('alt', 'Me gusta');
       }
     });
-
+ 
     this._deleteButton.addEventListener('click', () => {
       this._element.remove();
     });
-
+ 
     this._image.addEventListener('click', () => {
       this._handleImageClick(this._link, this._name);
     });
   }
-
+ 
   _handleImageClick(link, name) {
     const popupImage = document.querySelector('.popup_type_image');
     const popupImg = popupImage.querySelector('.popup__image');
     const popupCaption = popupImage.querySelector('.popup__caption');
-
+ 
     popupImg.src = link;
     popupImg.alt = name;
     popupCaption.textContent = name;
-
+ 
     popupImage.classList.add('popup_opened');
   }
-
+ 
   generateCard() {
     this._element = this._getTemplate();
-
+ 
     this._image = this._element.querySelector('.card__image');
     this._title = this._element.querySelector('.card__title');
     this._likeButton = this._element.querySelector('.card__like-button');
     this._likeIcon = this._likeButton.querySelector('img');
     this._deleteButton = this._element.querySelector('.card__delete-button');
-
+ 
     this._image.src = this._link;
     this._image.alt = this._name;
     this._title.textContent = this._name;
-
+ 
     this._setEventListeners();
-
+ 
     return this._element;
   }
 }
-
+ 
+ 
--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,33 @@
-import { Card } from './Card.js';
-import { FormValidator } from './FormValidator.js';
-
+import { Card } from './components/Card.js';
+import { FormValidator } from './components/FormValidator.js';
+ 
 const popup = document.querySelector('.popup');
 const openButton = document.querySelector('.profile__edit-button');
 const closeButton = popup.querySelector('.popup__close-button');
-
+ 
 const nameInput = document.querySelector('.popup__input[name="name"]');
 const jobInput = document.querySelector('.popup__input[name="about"]');
 const nameDisplay = document.querySelector('.profile__name');
 const jobDisplay = document.querySelector('.profile__description');
 const formElement = document.querySelector('.popup__form');
-
+ 
 openButton.addEventListener('click', () => {
   nameInput.value = nameDisplay.textContent;
   jobInput.value = jobDisplay.textContent;
   popup.classList.add('popup_opened');
 });
-
+ 
 closeButton.addEventListener('click', () => {
   popup.classList.remove('popup_opened');
 });
-
+ 
 formElement.addEventListener('submit', (evt) => {
   evt.preventDefault();
   nameDisplay.textContent = nameInput.value;
   jobDisplay.textContent = jobInput.value;
   popup.classList.remove('popup_opened');
 });
-
+ 
 const initialCards = [
   { name: "Valle de Yosemite", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg" },
   { name: "Lago Louise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg" },
@@ -36,31 +36,31 @@ const initialCards = [
   { name: "Parque Nacional de la Vanoise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg" },
   { name: "Lago di Braies", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg" }
 ];
-
+ 
 const galleryContainer = document.querySelector('.gallery');
 initialCards.forEach((cardData) => {
   const card = new Card(cardData, '#card-template');
   const cardElement = card.generateCard();
   galleryContainer.append(cardElement);
 });
-
-
+ 
+ 
 const addButton = document.querySelector('.profile__add-button');
 const addPopup = document.querySelector('.popup_type_add');
 const addCloseButton = addPopup.querySelector('.popup__close-button');
 const addForm = addPopup.querySelector('.popup__form');
 const titleInput = addForm.querySelector('input[name="title"]');
 const linkInput = addForm.querySelector('input[name="image"]');
-
+ 
 addButton.addEventListener('click', () => {
   addForm.reset();
   addPopup.classList.add('popup_opened');
 });
-
+ 
 addCloseButton.addEventListener('click', () => {
   addPopup.classList.remove('popup_opened');
 });
-
+ 
 addForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const newCard = {
@@ -72,23 +72,23 @@ const cardElement = card.generateCard();
   galleryContainer.prepend(cardElement);
   addPopup.classList.remove('popup_opened');
 });
-
+ 
 function openImagePopup(src, alt) {
   const popupImage = document.querySelector('.popup_type_image');
   const popupImg = popupImage.querySelector('.popup__image');
   const popupCaption = popupImage.querySelector('.popup__caption');
-
+ 
   popupImg.src = src;
   popupImg.alt = alt;
   popupCaption.textContent = alt;
-
+ 
   popupImage.classList.add('popup_opened');
 }
-
+ 
 document.querySelector('.popup_type_image .popup__close-button').addEventListener('click', () => {
   document.querySelector('.popup_type_image').classList.remove('popup_opened');
 });
-
+ 
 const validationConfig = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -97,13 +97,13 @@ const validationConfig = {
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 };
-
+ 
 const formList = Array.from(document.querySelectorAll(validationConfig.formSelector));
 formList.forEach((formElement) => {
   const formValidator = new FormValidator(validationConfig, formElement);
   formValidator.enableValidation();
 });
-
+ 
 document.querySelectorAll('.popup').forEach((popup) => {
   popup.addEventListener('mousedown', (event) => {
     if (event.target === popup) {
@@ -119,7 +119,3 @@ document.addEventListener('keydown', (event) => {
     }
   }
 });
-
-
-
-
